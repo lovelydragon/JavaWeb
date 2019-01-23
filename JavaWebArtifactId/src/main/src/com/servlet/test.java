b5894014c44642609eb7213015ae86f4a2bb4967
lovelrdragon <747416462@qq.com>
2019-01-23 17:38:22 +0800

    Maven

---
+++ JavaWebArtifactId/src/main/src/com/servlet/test.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class test implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("doFilter");
+        servletRequest.getRequestDispatcher("/index.jsp").forward(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
