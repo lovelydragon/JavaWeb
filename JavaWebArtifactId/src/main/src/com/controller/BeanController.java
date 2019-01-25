175d783e7a6b904726039f59f7cec96cd00ea27a
lovelrdragon <747416462@qq.com>
2019-01-25 17:40:23 +0800

    spring mvc

---
+++ JavaWebArtifactId/src/main/src/com/controller/BeanController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BeanController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        System.out.println("beanController");
+        return modelAndView;
+    }
+}
---
+++ JavaWebArtifactId/src/main/src/com/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "login")
+public class LoginController {
+
+    @RequestMapping(value = "test")
+    public String test(){
+        System.out.println("test");
+        return "index";
+    }
+}
